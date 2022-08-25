14071caceb07542605fdfa415f376a054a607b06
Daniel-Santin <32037139@mackenzista.com.br>
2022-08-25 16:14:31 -0300

    v

---
+++ js/script.js
@@ -0,0 +1,60 @@
+const pokemon_name = document.querySelector(".pokemon_name");
+const pokemon_number = document.querySelector(".pokemon_number");
+const pokemon_img = document.querySelector(".pokemon_img");
+
+const form  = document.querySelector(".form");
+const input = document.querySelector(".input_search");
+
+const prevBt  = document.querySelector(".btn-prev");
+const nextBt = document.querySelector(".btn-next");
+
+let searchPKM = 1;
+
+const fetchPokemon = async (pokemon) => {
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    if (APIResponse.status == 200){
+    const data = await APIResponse.json();
+    return data;
+    }
+}
+
+const renderPokemon = async (pokemon) =>{
+
+    pokemon_name.innerHTML = "Loading ...";
+    pokemon_number.innerHTML = "";
+
+    const data = await fetchPokemon(pokemon);
+
+    if(data){
+        pokemon_name.innerHTML = data.name;
+        pokemon_number.innerHTML = data.id + " – ";
+        pokemon_img.src = data["sprites"]["versions"]["generation-vii"]["ultra-sun-ultra-moon"]["front_shiny"];
+        input.value = "";
+        searchPKM = data.id;
+    }else{
+        pokemon.style.display = "none"
+        pokemon_name.innerHTML = "Not Found ";
+        pokemon_number.innerHTML = "";
+    }
+}
+
+
+form.addEventListener("submit",(event) => {
+    event.preventDefault();
+    renderPokemon(input.value.toLowerCase());
+});
+
+prevBt.addEventListener("click",() => {
+    if(searchPKM > 1){
+    searchPKM -=1
+    renderPokemon(searchPKM );
+    }
+});
+
+nextBt.addEventListener("click",() => {
+    
+    searchPKM +=1
+    renderPokemon(searchPKM );
+});
+
+renderPokemon(searchPKM);
